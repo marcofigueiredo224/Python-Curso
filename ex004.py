32c171909d8725fd30b8705a71c1f48da92ab07e
marcofigueiredo224 <85959043+marcofigueiredo224@users.noreply.github.com>
2021-06-25 13:58:08 -0300

    Aula 04

---
+++ ex004.py
@@ -0,0 +1,8 @@
+n = int(input('Digite um número: '))
+d = n * 2
+t = n * 3
+r = n ** (1/2)
+print('O dobro de {} vale {} '.format(n, d))
+print ('O triplo vale {}'.format(t))
+print ('A raíz quadrada vale {:.2f}'.format(r))
+
