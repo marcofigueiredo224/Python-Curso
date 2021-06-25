df21946e1d15050ebe6e3163b52d8a7360d53cab
marcofigueiredo224 <85959043+marcofigueiredo224@users.noreply.github.com>
2021-06-25 13:57:50 -0300

    Aula 02

---
+++ ex002.py
@@ -0,0 +1,13 @@
+a = int(input("Digite Algo: "))
+print ("O tipo primitivo desse valor é ",(type(a)))
+print("Só tem espaços?",a.isspace())
+print("É alfabético?",a.isalpha())
+print("É alfanumérico?",a.isalnum())
+print("Está em maiúsculas?",a.isupper())
+print("Está em minúsculas?",a.islower())
+print("Está capitalizada?",a.istitle())
+
+
+
+
+
